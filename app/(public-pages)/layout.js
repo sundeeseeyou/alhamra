9ddf7f13dc384f563b84f9170399a26513bd83ee
Sandi Miftah <sandiseeyou@gmail.com>
2026-01-27 19:30:06 +0700

    feat(config): initial update

--- app/(public-pages)/layout.js
+++ app/(public-pages)/layout.js
@@ -1,22 +1,22 @@
-import Banner from '@/components/banner';
-import Footer from '@/components/footer';
-import Navbar from '@/components/navbar';
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+import favicon from "@/app/favicon.ico";
 
 export const metadata = {
-    title: 'Material - PrebuiltUI',
-    description: 'Material is a powerful AI-powered platform that allows you to create and edit websites, blogs, and more. With Material, you can easily create a professional-looking website without any coding knowledge.',
-    appleWebApp: {
-        title: 'Material - PrebuiltUI',
-    },
+  title: "Al-Hamra | Trade and Development",
+  description: "Indonesia B2B E-commerce Platform",
+  appleWebApp: {
+    title: "Al-Hamra | Trade and Development",
+    icon: favicon,
+  },
 };
 
 export default function Layout({ children }) {
-    return (
-        <>
-            <Banner />
-            <Navbar />
-            {children}
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
 }
